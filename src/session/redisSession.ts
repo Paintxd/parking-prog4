1dac82079e2c452552fcb9caf43b773fc05ad262
Paint <gima69xd@gmail.com>
2021-10-09 01:20:14 -0300

    redis connection

--- src/session/redisSession.ts
+++ src/session/redisSession.ts
@@ -1,8 +1,18 @@
+import { URL } from 'url';
 import * as session from 'express-session';
 import * as redis from 'redis';
 
 const RedisStore = require('connect-redis')(session);
-const redisClient = redis.createClient();
+let redisClient;
+
+if (process.env.REDISTOGO_URL) {
+  const rtg = new URL(process.env.REDISTOGO_URL);
+
+  redisClient = redis.createClient(Number(rtg.port), rtg.hostname);
+  redisClient.auth(rtg.password);
+} else {
+  redisClient = redis.createClient();
+}
 
 export class RedisSession {
   static getSession() {
