cf4f72cf887bca2a1418a0b8fed50aae463898e0
Paint <gima69xd@gmail.com>
2021-10-10 22:37:19 -0300

    fix: minor fixes

--- src/session/redisSession.ts
+++ src/session/redisSession.ts
@@ -3,19 +3,24 @@ import * as session from 'express-session';
 import * as redis from 'redis';
 
 const RedisStore = require('connect-redis')(session);
-let redisClient;
-
-if (process.env.REDISTOGO_URL) {
-  const rtg = new URL(process.env.REDISTOGO_URL);
-
-  redisClient = redis.createClient(Number(rtg.port), rtg.hostname);
-  redisClient.auth(rtg.password);
-} else {
-  redisClient = redis.createClient();
-}
 
 export class RedisSession {
+  private static getRedistClient() {
+    if (process.env.REDISTOGO_URL) {
+      const rtg = new URL(process.env.REDISTOGO_URL);
+
+      const redisClient = redis.createClient(Number(rtg.port), rtg.hostname);
+      redisClient.auth(rtg.password);
+
+      return redisClient;
+    }
+
+    return redis.createClient();
+  }
+
   static getSession() {
+    const redisClient = this.getRedistClient();
+
     return session({
       store: new RedisStore({
         client: redisClient,
