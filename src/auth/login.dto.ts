7a630596bd552cbd7471483697cb5e409820f23a
Daniel Souza <daniel.souza@unoesc.edu.br>
2021-10-13 21:27:25 -0300

    Estilo view part 1

--- src/auth/login.dto.ts
+++ src/auth/login.dto.ts
@@ -2,12 +2,12 @@ import { IsNotEmpty } from 'class-validator';
 
 export class LoginDto {
   @IsNotEmpty({
-    message: 'Obrgatorio informar um login',
+    message: 'Obrigatorio informar um login',
   })
   login: string;
 
   @IsNotEmpty({
-    message: 'Obrgatorio informar uma senha',
+    message: 'Obrigatorio informar uma senha',
   })
   password: string;
 }
